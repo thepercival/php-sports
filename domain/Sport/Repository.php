bb8673c2c44e7e19be07193bc1416dd6cc2eb9df
thepercival <coendunnink@gmail.com>
2021-02-02 09:06:19 +0100

    fetch sports

--- domain/Sport/Repository.php
+++ domain/Sport/Repository.php
@@ -2,12 +2,29 @@
 
 namespace Sports\Sport;
 
+use League\Period\Period;
+use Sports\Person as PersonBase;
 use Sports\Sport as SportBase;
+use Sports\Team;
 
 class Repository extends \Sports\Repository
 {
     public function find($id, $lockMode = null, $lockVersion = null): ?SportBase
     {
         return $this->_em->find($this->_entityName, $id, $lockMode, $lockVersion);
+    }
+
+    /**
+     * @param bool|null $withCustomId
+     * @return array|SportBase[]
+     */
+    public function findByExt(bool $withCustomId = null)
+    {
+        $qb = $this->createQueryBuilder('s');
+        if( $withCustomId !== null ) {
+            $operator = $withCustomId ? '>' : '=';
+            $qb = $qb->andWhere('s.customId ' . $operator .' 0' );
+        }
+        return $qb->getQuery()->getResult();
     }
 }
