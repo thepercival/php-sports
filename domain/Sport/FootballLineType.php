f2bac995dcec81b90d0859ce5f9184b7bc705d7e
thepercival <coendunnink@gmail.com>
2023-02-02 21:58:50 +0100

    fix(sport): add defaults

---
+++ domain/Sport/FootballLineType.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sports\Sport;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use SportsHelpers\EnumDbType;
+
+class FootballLineType extends EnumDbType
+{
+    public static function getNameHelper(): string
+    {
+        return 'enum_FootballLine';
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        if ($value === FootballLine::GoalKeeper->value) {
+            return FootballLine::GoalKeeper;
+        }
+        if ($value === FootballLine::Defense->value) {
+            return FootballLine::Defense;
+        }
+        if ($value === FootballLine::Midfield->value) {
+            return FootballLine::Midfield;
+        }
+        if ($value === FootballLine::Forward->value) {
+            return FootballLine::Forward;
+        }
+        return null;
+    }
+
+    public function getSQLDeclaration(array $column, AbstractPlatform $platform)
+    {
+        return 'int';
+    }
+}
