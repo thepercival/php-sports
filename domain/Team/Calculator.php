e03b63e949b7edfa6293e660df55f6b0679bf355
thepercival <coendunnink@gmail.com>
2023-03-12 12:04:13 +0100

    fix(againstGame): add teamCalculator

---
+++ domain/Team/Calculator.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Sports\Team;
+
+use Sports\Competition;
+use Sports\Competitor\StartLocationMap;
+use Sports\Competitor\Team as TeamCompetitor;
+use Sports\Team;
+use SportsHelpers\Against\Side;
+use Sports\Game\Against as AgainstGame;
+
+class Calculator
+{
+    protected StartLocationMap $startLocationMap;
+
+    public function __construct(Competition $competition)
+    {
+        $competitors = array_values($competition->getTeamCompetitors()->toArray());
+        $this->startLocationMap = new StartLocationMap($competitors);
+    }
+
+    /**
+     * @param AgainstGame $againstGame
+     * @return list<Team>
+     */
+    public function getTeams(AgainstGame $againstGame): array {
+        $teams = [];
+        foreach ([Side::Home, Side::Away] as $side) {
+            try {
+                $teams[] = $this->getSingleTeam($againstGame, $side);
+            } catch( \Exception $e ) {
+
+            }
+        }
+        return $teams;
+    }
+
+    public function getSingleTeam(AgainstGame $againstGame, Side $side): Team
+    {
+        foreach ($againstGame->getSidePlaces($side) as $gamePlace) {
+            $startLocation = $gamePlace->getPlace()->getStartLocation();
+            if ($startLocation === null) {
+                throw new \Exception('startlocation could not be found', E_ERROR);
+            }
+            /** @var TeamCompetitor|null $teamCompetitor */
+            $teamCompetitor = $this->startLocationMap->getCompetitor($startLocation);
+            if ($teamCompetitor === null) {
+                throw new \Exception('team could not be found', E_ERROR);
+            }
+            return $teamCompetitor->getTeam();
+
+
+        }
+        throw new \Exception('team not found', E_ERROR);
+    }
+//
+//foreach ($gamePlace->getParticipations() as $gameParticipation) {
+//$teamPlayer = $gameParticipation->getPlayer();
+//if ($teamPlayer->getTeam() !== $teamCompetitor->getTeam()) {
+//$message = 'teams of player and gameparticipation are not equal';
+//$this->getLogger()->error($message);
+//continue;
+//}
+//
+//if (!$teamPlayer->getPeriod()->contains($game->getStartDateTime())) {
+//    $message = 'game is outside playerperiod ' . $teamPlayerOutput->getString(
+//            $teamPlayer,
+//            ''
+//        );
+//    $this->getLogger()->error($message);
+//    continue;
+//}
+//if (!$seasonPeriod->contains($teamPlayer->getPeriod())) {
+//    $message = 'player-period ' . $teamPlayerOutput->getString(
+//            $teamPlayer,
+//            ''
+//        ) . ' is outside season ' . $seasonPeriod->toIso80000('Y-m-d');
+//    $this->getLogger()->error($message);
+//}
+//}
+}
